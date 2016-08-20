4496572cc4d41697e2795d28627fac06e7b40b20
Adam Bright <Brighto@Adams-MacBook-Air.local>
2016-08-19 17:37:19 -0700

    Reverting to original.

--- ex22.py
+++ ex22.py
@@ -32,3 +32,4 @@ def print_first_and_last_sorted(sentence):
     """Sorts the words then prints the first and last one."""
     words = sort_sentence(sentence)
     print_first_word(words)
+    print_last_word(words)
